8c927d2d2b9643471940090d1a74cc203c123b68
btfrankenstein <btfrankenstein@gmail.com>
2016-12-30 16:52:04 +0800

    添加注释

--- demo/drag.js
+++ demo/drag.js
@@ -1,14 +1,15 @@
-;(function(window, undefined) {
+;
+(function(window, undefined) {
 
     var methods = {
-        on: function(node, eventname, handler) {
+        on: function(node, eventname, handler) { // 注册事件
             if (node.addEventListener) {
                 node.addEventListener(eventname, handler)
             } else {
                 node.attachEvent('on' + eventname, handler)
             }
         },
-        getStyle: function(node, stylename) {
+        getStyle: function(node, stylename) { // 获取元素样式
             var realStyle
             if (window.getComputedStyle) {
                 realStyle = window.getComputedStyle(node)[stylename]
@@ -17,14 +18,14 @@
             }
             return realStyle
         },
-        setCss: function(node, css) {
+        setStyle: function(node, css) { // 为元素添加css样式
             for (var key in css) {
                 node.style[key] = css[key]
             }
         }
     }
 
-    function DragElement(node) {
+    function DragElement(node) { // 被拖拽的对象
         this.node = node
         this.x = 0
         this.y = 0
@@ -32,29 +33,29 @@
 
     DragElement.prototype = {
         constructor: DragElement,
-        setXY: function(x, y) {
+        setXY: function(x, y) { // 对象被拖拽前，记录坐标值。x-横坐标，y-纵坐标
             this.x = parseInt(x) || 0
             this.y = parseInt(y) || 0
             return this
         },
-        setEleCss: function(css) {
-            methods.setCss(this.node, css)
+        setEleCss: function(css) { // 为被拖拽的对象添加css样式
+            methods.setStyle(this.node, css)
             return this
         }
     }
 
-    function Mouse() {
+    function Mouse() { // 鼠标对象
         this.x = 0
         this.y = 0
     }
 
-    Mouse.prototype.setXY = function(x, y) {
+    Mouse.prototype.setXY = function(x, y) { // 拖拽对象前记录鼠标的坐标
         this.x = parseInt(x)
         this.y = parseInt(y)
     }
 
     var draggableConfig = {
-        zIndex: 1,
+        // zIndex: 1,
         draggingObj: null,
         mouse: new Mouse(),
         diffWidth: '',
@@ -62,24 +63,25 @@
     }
 
     function Drag(ele, outer) {
+        // 计算被拖拽元素和其容器的宽高差
         draggableConfig.diffWidth = parseInt(methods.getStyle(outer, 'width')) - parseInt(methods.getStyle(ele, 'width'))
         draggableConfig.diffHeight = parseInt(methods.getStyle(outer, 'height')) - parseInt(methods.getStyle(ele, 'height'))
 
-        function mouseDown(event) {
+        function mouseDown(event) { // 鼠标按下时的handler
             var ele = event.target || event.srcElement
 
             draggableConfig.mouse.setXY(event.clientX, event.clientY)
             draggableConfig.draggingObj = new DragElement(ele)
             draggableConfig.draggingObj.setXY(ele.style.left, ele.style.top).setEleCss({
-                'zIndex': draggableConfig.zIndex++,
-                'position': 'relative'
+                // 'zIndex': draggableConfig.zIndex++,
+                'position': 'relative' // position是relative或者absolute， left和right才有效果
             })
         }
         methods.on(ele, 'mousedown', mouseDown)
     }
 
 
-    methods.on(document, 'mousemove', function(event) {
+    methods.on(document, 'mousemove', function(event) { // 鼠标移动时的handler
 
         if (draggableConfig.draggingObj) {
             var mouse = draggableConfig.mouse
@@ -110,9 +112,10 @@
 
     })
 
-    methods.on(document, 'mouseup', function(event) {
+    methods.on(document, 'mouseup', function(event) { // 鼠标抬起时的handler
         draggableConfig.draggingObj = null
     })
 
     window.Drag = Drag
+
 })(window)
